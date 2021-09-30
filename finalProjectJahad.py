1498a3f027a2de80b1cf6d2fb09aa4644e2eabe2
Sarvin Nami <srvn0nm@gmail.com>
2021-09-30 18:31:22 +0330

    debugging

--- finalProjectJahad.py
+++ finalProjectJahad.py
@@ -20,14 +20,14 @@ class Person :
             self.ID = ID
     def showInfo(self) :
         if self.gender == 'male' or self.gender == 'Male' or self.gender == 'm' or self.gender == 'M':
-            return '\nMr.' + self.name + ' ' + self.family + ' has ' +str(self.phoneNumber) + " as their phone number" + f' and {self.ID} is their ID number'
+            return '\nMr.' + self.name + '  ' + self.family + ' has ' +str(self.phoneNumber) + " as their phone number" + f' and {self.ID} is their ID number'
         elif self.gender == 'female' or self.gender == 'Female' or self.gender == 'f' or self.gender == 'F' : 
-            return '\nMiss.' + ' ' + self.name + self.family + ' has ' +str(self.phoneNumber) + " as their phone number" + f' and {self.ID} is their ID number'
+            return '\nMiss.' + self.name + '  ' + self.family + ' has ' +str(self.phoneNumber) + " as their phone number" + f' and {self.ID} is their ID number'
 class NewAccountCreators(Person) :
     def __init__(self, name, family, phoneNumber, gender, ID, moneyAmount) :
         super().__init__(name, family, phoneNumber, gender, ID)
         self.moneyAmount = moneyAmount
-        print(f'Congratulations! Your new account created with {self.moneyAmount}$ successfully! \n')
+        print(f'\nCongratulations! Your new account created with {self.moneyAmount}$ successfully! \n')
     def showInfo(self) : return super().showInfo() + f' has created a new acoount with {self.moneyAmount}$ balance.\n'
 class Sponsors(Person) :
     def __init__(self,name, family, phoneNumber, gender, ID, loaner, balance, loanAmount) : 
@@ -47,9 +47,9 @@ class Loaners(Person) :
         self.installmentPeriod = installmentPeriod
         self.balance = balance
         self.payment = None
-        print(f'You loan {self.loanAmount}$ successfully and have {self.installmentPeriod} months to pay it back.\n')
-        s1 = Sponsors(input('Please type your name here : '),input('Please type your family here : '),input('Please type your phone number here : '),input('Please type your gender here m or f : '),input('Please type your ID number here : '),self.name,int(input('Please type your account balance here : ')),self.loanAmount)
-        s2 = Sponsors(input('Please type your name here : '),input('Please type your family here : '),input('Please type your phone number here : '),input('Please type your gender here m or f : '),input('Please type your ID number here : '),self.name,int(input('Please type your account balance here : ')),self.loanAmount)
+        print(f'\nYou loan {self.loanAmount}$ successfully and have {self.installmentPeriod} months to pay it back.\nnow your sponsors should register :\n')
+        self.s1 = Sponsors(input('Please type your name here : '),input('Please type your family here : '),input('Please type your phone number here : '),input('Please type your gender here m or f : '),input('Please type your ID number here : '),self.name,int(input('Please type your account balance here : ')),self.loanAmount)
+        self.s2 = Sponsors(input('Please type your name here : '),input('Please type your family here : '),input('Please type your phone number here : '),input('Please type your gender here m or f : '),input('Please type your ID number here : '),self.name,int(input('Please type your account balance here : ')),self.loanAmount)
     def loanPayments(self) : 
         if self.balance >= self.loanAmount/self.installmentPeriod :
             self.balance -= self.loanAmount/self.installmentPeriod
@@ -58,20 +58,20 @@ class Loaners(Person) :
         else :
             print('You don\'t have enough money to pay it back!\n')
     def showInfo(self) : 
-        return super().showInfo() + f' has got {self.loanAmount}$ loan with {self.installmentPeriod} months installment period which {self.payment} months remains.\n' + 'There are their sponsors:\n' + s1.showInfo() + '\n' + s2.showInfo
+        return super().showInfo() + f' has got {self.loanAmount}$ loan with {self.installmentPeriod} months installment period which {self.payment} months remains.\n' + 'There are their sponsors:\n' + f'{self.s1.showInfo()}\n{self.s2.showInfo()}'
 class AppInstallers(Person):
     def __init__(self, name, family, phoneNumber, gender, ID, usage) :
-        super().__init__(name, family, phoneNumber, gender, ID, usage)
+        super().__init__(name, family, phoneNumber, gender, ID)
         self.usage = usage
-        print(f'Congratulations! You have successfully insalled our app for {self.usage}.\n' + 'You can now install your app now.')
+        print(f'Congratulations! You have successfully insalled our app for {self.usage}.\n')
     def showInfo(self):
-        return super().showInfo() + f' has bank application for {self.usage}.\n' + 'You can now install your app now.'
+        return super().showInfo() + f' has bank application for {self.usage}.\n'
 p = Person(input('Please enter your name here : '),input('Please enter your family name here : '),input('Please enter your phone number here : '),input('Please enter your gender here m or f : '), input('Please enter your ID number here : '))
 def menu():
     print('\n1)Create a new account\n2)show the new account information\n3)loan\n4)show the loan information\n5)install the bank application\n6)show the bank application information\n7)pay loan bill\n8)exit\n')
 menu()
 choice =input('Please enter your choice here : ')
-while choice != 'Exit' or choice !='exit' or choice != '8' :
+while True :
     if choice == '1' : 
         c = NewAccountCreators(p.name,p.family,p.phoneNumber,p.gender,p.ID,int(input('Please type the amount of money you want to add to you new account here : ')))
         menu()
@@ -100,4 +100,6 @@ while choice != 'Exit' or choice !='exit' or choice != '8' :
         l.loanPayments()
         menu()
         choice =input('Please enter your choice here : ')
-else : print(f'\nGoodbye {p.name} {p.family}. Have a nice day! ^-^\n') 
+    if choice == '8'or choice == 'exit' or choice == 'Exit' :
+        print(f'\nGoodbye {p.name} {p.family}. Have a nice day! ^-^\n') 
+        break
