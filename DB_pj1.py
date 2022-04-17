a5d173865e455a235721923eaef7bd9baa3cfe8c
Sarvin Nami <srvn0nm@gmail.com>
2022-04-17 14:47:20 +0430

    first successful conection

--- DB_pj1.py
+++ DB_pj1.py
@@ -1,8 +1,8 @@
 import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="root",
+  password="rootSarvin1372"
 )
 
 print(mydb)
