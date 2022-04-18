6b6a146af890a1df9eb053fe72cbce42258a0f4f
Sarvin Nami <srvn0nm@gmail.com>
2022-04-18 10:36:32 +0430

    adding variables in tables

--- DB_pj1.py
+++ DB_pj1.py
@@ -7,5 +7,33 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE ST (STID VARCHAR('s1','s2','s3','s4','s5'), STname VARCHAR('Sn1','Sn2','Sn3','Sn4','Sn5'),STLevel VARCHAR(),STmajor VARCHAR())")
-mycursor.execute("CREATE TABLE CO (COID VARCHAR('c1','c2','c3','c4'), COname VARCHAR('Cn1','Cn2','Cn3','Cn4'), COtype VARCHAR('P','P','Q','Q'),Credit VARCHAR(4,3,4,3))")
+# mycursor.execute("CREATE TABLE ST (STID VARCHAR(255), STname VARCHAR(255),STLevel VARCHAR(255),STmajor VARCHAR(255))")
+# mycursor.execute("CREATE TABLE CO (COID VARCHAR(255), COname VARCHAR(255), COtype VARCHAR(255),Credit VARCHAR(255))")
+# mycursor.execute("CREATE TABLE STCO (STID VARCHAR(255),COID VARCHAR(255), YR VARCHAR(255), TR VARCHAR(255),Grade VARCHAR(255))")
+# inserting into table st
+# sql = "INSERT INTO ST (STID, STname) VALUES (%s, %s)"
+# val = [
+#   ('S1', 'Sn1'),
+#   ('S2', 'Sn2'),
+#   ('S3', 'Sn3'),
+#   ('S4', 'Sn4'),
+#   ('S5', 'Sn5')
+# ]
+
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
+
+# sql2 = "INSERT INTO CO (COID,COname, COtype,Credit) VALUES (%s, %s, %s, %d)"
+# val2 = [
+#   ('C1', 'Cn1', 'P', 4),
+#   ('C2', 'Cn2', 'P', 3),
+#   ('C3', 'Cn3', 'Q', 4),
+#   ('C4', 'Cn4', 'Q', 3)
+# ]
+
+# mycursor.executemany(sql2, val2)
+
+# mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-print(mydb)
+sql = "INSERT INTO ST (STID, STname) VALUES (%s, %s)"
+# val = [
+#   ('S1', 'Sn1'),
+#   ('S2', 'Sn2'),
+#   ('S3', 'Sn3'),
+#   ('S4', 'Sn4'),
+#   ('S5', 'Sn5')
+# ]
+
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
