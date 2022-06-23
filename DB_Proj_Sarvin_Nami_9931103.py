0f28486bfd26dfd1fa708919eb6fc693fcc592a1
Sarvin Nami <srvn0nm@gmail.com>
2022-06-23 14:21:02 +0430

    create table users

--- DB_Proj_Sarvin_Nami_9931103.py
+++ DB_Proj_Sarvin_Nami_9931103.py
@@ -4,3 +4,7 @@ db = mysql.connect(
     user = "root",
     passwd = "rootSarvin1372"
 )
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE datacamp")
+
+cursor.execute("CREATE TABLE users (uname VARCHAR(255), ulname  VARCHAR(255), uID VARCHAR(255)), phone VARCHAR(255), email VARCHAR(255), upassword VARCHAR(255), useccheck VARCHAR(255), time VARCHAR(255), limited_login Int, limited_password Int")
