4e7d806603f7c6d88420562b432d32493c12186e
Sarvin Nami <srvn0nm@gmail.com>
2022-06-23 14:24:39 +0430

    create table blocked

--- DB_Proj_Sarvin_Nami_9931103.py
+++ DB_Proj_Sarvin_Nami_9931103.py
@@ -9,3 +9,4 @@ cursor.execute("CREATE DATABASE datacamp")
 
 cursor.execute("CREATE TABLE users (uname VARCHAR(255), ulname  VARCHAR(255), uID Int, phone VARCHAR(255), email VARCHAR(255), upassword VARCHAR(255), useccheck VARCHAR(255), time VARCHAR(255), limited_login Int, limited_password Int")
 cursor.execute("CREATE TABLE friends (u1ID Int, u2ID Int, fID Int")
+cursor.execute("CREATE TABLE blocked (blockerID Int, blockedID Int, bID Int, time VARCHAR(255)")
