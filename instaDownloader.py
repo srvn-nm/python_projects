039bbd129368615748d8f45ab2a0e4c1b203f87c
Sarvin Nami <srvn0nm@gmail.com>
2022-07-02 18:24:32 +0430

    adding instaloader

--- instaDownloader.py
+++ instaDownloader.py
@@ -1,21 +1,25 @@
 from tkinter import *
 import instaloader
 
+# instagram
 insta = instaloader.Instaloader()
 
+# window
 window = Tk()
 window.geometry("600x600")
 window.maxsize(800,800)
 window.minsize(400,400)
 window.title("Instagram Profile Downloader")
 
-label = Label(window,text="Hello Instagram!")
+label = Label(window,text="Type the username here: ")
 label.pack()
 
 def buttonFunc():
-    print("You pressed the button! >-<")
+    # print("You pressed the button! >-<")
     button.config(text="Downloaded Already!")
-    label.config(text=input.get())
+    profile = instaloader.Profile.from_username(insta.context,input.get())
+    print(profile.followers)
+    # label.config(text=input.get())
 
 button = Button(window,text="Click here to download",fg="white",bg="black",command=buttonFunc)
 button.pack()
@@ -23,5 +27,6 @@ button.pack()
 
 input = Entry(window)
 input.pack()
+
 
 window.mainloop()
