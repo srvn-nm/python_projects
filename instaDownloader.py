a05c7b1a44060bae7bf38dff6a7db17c30870b2e
Sarvin Nami <srvn0nm@gmail.com>
2022-07-02 12:03:03 +0430

    button creating and features

--- instaDownloader.py
+++ instaDownloader.py
@@ -5,6 +5,11 @@ window.geometry("600x600")
 window.maxsize(800,800)
 window.minsize(400,400)
 window.title("Instagram Profile Downloader")
+
 label = Label(window,text="Hello Instagram!")
 label.pack()
+
+button = Button(window,text="Click here to download",fg="white",bg="black")
+button.pack()
+
 window.mainloop()
