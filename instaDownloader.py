a34b220b12470f4922008c25277b73ae310925b5
Sarvin Nami <srvn0nm@gmail.com>
2022-07-02 11:55:41 +0430

    max size

--- instaDownloader.py
+++ instaDownloader.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.geometry("600x600")
+window.maxsize(800,800)
 window.title("Instagram Profile Downloader")
 label = Label(window,text="Hello Instagram!")
 label.pack()
