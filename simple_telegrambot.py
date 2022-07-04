97c0b2566215900489b312110e20ec9858d36abf
Sarvin Nami <srvn0nm@gmail.com>
2022-07-04 15:55:55 +0430

    simple commands

--- simple_telegrambot.py
+++ simple_telegrambot.py
@@ -3,3 +3,9 @@ import telebot
 TOKEN = '5528897472:AAEVbf7Ic7wGOE3sYXd0v0j7cAtSUGByhHU'
 
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Hello, I'm Sarvin. What can I do for you?")
+ 
+bot.infinity_polling()
