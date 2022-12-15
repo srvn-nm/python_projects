5bc5b75769c7d47dfec4b42fbbf51f42cf56017a
Sarvin Nami <srvn0nm@gmail.com>
2022-12-16 01:37:50 +0330

    the very first query to add to the student table

--- DB_LAB_HW1_Sarvin_Nami_9931103.py
+++ DB_LAB_HW1_Sarvin_Nami_9931103.py
@@ -11,12 +11,21 @@ cursor = db.cursor()
 # cursor.execute("CREATE DATABASE school")
 cursor.execute("use school")
 
+cursor.execute("DROP TABLE IF EXISTS Term")
 cursor.execute("DROP TABLE IF EXISTS Student")
 cursor.execute("DROP TABLE IF EXISTS Teacher")
 cursor.execute("DROP TABLE IF EXISTS Course")
-cursor.execute("DROP TABLE IF EXISTS Term")
 
 cursor.execute("CREATE TABLE Student (s_name VARCHAR(255) not NULL, s_family  VARCHAR(255) not NULL, s_id INT NOT NULL UNIQUE, age INT not NULL, city VARCHAR(255) not NULL , s_field VARCHAR(255) not NULL, gender VARCHAR(255) NOT NULL, tavg INT DEFAULT 0, PRIMARY KEY(s_id))")
 cursor.execute("CREATE TABLE Teacher (t_name VARCHAR(255) not NULL, t_family  VARCHAR(255) not NULL, t_id INT NOT NULL UNIQUE, age INT not NULL, t_field VARCHAR(255) not NULL, salary INT DEFAULT 0, PRIMARY KEY(t_id))")
 cursor.execute("CREATE TABLE Course (c_name VARCHAR(255) not NULL, dep  VARCHAR(255) not NULL, c_id INT NOT NULL UNIQUE, unit INT not NULL, c_field VARCHAR(255) not NULL, PRIMARY KEY(c_id))")
 cursor.execute("CREATE TABLE Term (id INT UNIQUE not NULL, s_id INT NOT NULL, t_id INT NOT NULL, c_id INT NOT NULL, grade INT NOT NULL, term_no INT, PRIMARY KEY(id), FOREIGN KEY(s_id) REFERENCES Student(s_id), FOREIGN KEY(t_id) REFERENCES Teacher(t_id), FOREIGN KEY(c_id) REFERENCES Course(c_id))")
+
+
+try:
+        Q1 ="INSERT INTO Student (s_name, s_family, s_id, age, city, s_field, gender, tavg) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = ("danial", "jahed", 1, 23, "mashhad", "software", "m", 18.26)
+        cursor.execute(Q1, values)
+        db.commit()
+except Exception as e:
+        print(e)
