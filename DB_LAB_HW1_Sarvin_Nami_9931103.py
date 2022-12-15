2597299e99c9cc57c4dafc00ca6e95657991d8b7
Sarvin Nami <srvn0nm@gmail.com>
2022-12-16 01:00:37 +0330

    creating table students

--- DB_LAB_HW1_Sarvin_Nami_9931103.py
+++ DB_LAB_HW1_Sarvin_Nami_9931103.py
@@ -9,3 +9,10 @@ db = mysql.connect(
 )
 cursor = db.cursor()
 cursor.execute("CREATE DATABASE school")
+
+cursor.execute("DROP TABLE IF EXISTS Student")
+cursor.execute("DROP TABLE IF EXISTS Teacher")
+cursor.execute("DROP TABLE IF EXISTS Course")
+cursor.execute("DROP TABLE IF EXISTS Term")
+
+cursor.execute("CREATE TABLE Student (s_name VARCHAR(255) not null, s_family  VARCHAR(255) not null, s_id INT NOT NULL UNIQUE, age INT not null , city VARCHAR(255) not null , s_field VARCHAR(255) not null, gender VARCHAR(255) NOT NULL, tavg INT DEFAULT 0, PRIMARY KEY(userID))")
