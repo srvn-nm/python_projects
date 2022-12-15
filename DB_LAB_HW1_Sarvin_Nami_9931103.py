e6ef4c5205518d040f3fe30985c83af5876c8172
Sarvin Nami <srvn0nm@gmail.com>
2022-12-16 01:05:03 +0330

    creating the table course

--- DB_LAB_HW1_Sarvin_Nami_9931103.py
+++ DB_LAB_HW1_Sarvin_Nami_9931103.py
@@ -17,3 +17,5 @@ cursor.execute("DROP TABLE IF EXISTS Term")
 
 cursor.execute("CREATE TABLE Student (s_name VARCHAR(255) not null, s_family  VARCHAR(255) not null, s_id INT NOT NULL UNIQUE, age INT not null , city VARCHAR(255) not null , s_field VARCHAR(255) not null, gender VARCHAR(255) NOT NULL, tavg INT DEFAULT 0, PRIMARY KEY(s_id))")
 cursor.execute("CREATE TABLE Teacher (t_name VARCHAR(255) not null, t_family  VARCHAR(255) not null, t_id INT NOT NULL UNIQUE, age INT not null , t_field VARCHAR(255) not null, salary INT DEFAULT 0, PRIMARY KEY(t_id))")
+cursor.execute("CREATE TABLE Course (c_name VARCHAR(255) not null, dep  VARCHAR(255) not null, c# INT NOT NULL UNIQUE, unit INT not null , c_field VARCHAR(255) not null, PRIMARY KEY(c#))")
+cursor.execute("CREATE TABLE Term (s_name VARCHAR(255) not null, s_family  VARCHAR(255) not null, s_id INT NOT NULL UNIQUE, age INT not null , city VARCHAR(255) not null , s_field VARCHAR(255) not null, gender VARCHAR(255) NOT NULL, tavg INT DEFAULT 0, PRIMARY KEY(s_id))")
