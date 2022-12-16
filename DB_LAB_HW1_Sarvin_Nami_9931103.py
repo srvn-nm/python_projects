f6c999ad20197d0b0a829d654afb1ecbee3b72f1
Sarvin Nami <srvn0nm@gmail.com>
2022-12-16 11:34:14 +0330

    creating a function to add to the course table

--- DB_LAB_HW1_Sarvin_Nami_9931103.py
+++ DB_LAB_HW1_Sarvin_Nami_9931103.py
@@ -33,14 +33,24 @@ def student_add(s_name, s_family, s_id, age, city, s_field, gender, tavg):
         
 def teacher_add(t_name, t_family, t_id, age, t_field, salary):
     try:
-        Q1 ="INSERT INTO Teacher (t_name, t_family, t_id, age, t_field, salary) VALUES(%s, %s, %s, %s, %s, %s)"
+        Q1 ="INSERT INTO Course (t_name, t_family, t_id, age, t_field, salary) VALUES(%s, %s, %s, %s, %s, %s)"
         values = (t_name, t_family, t_id, age, t_field, salary)
         cursor.execute(Q1, values)
         db.commit()
     except Exception as e:
         print(e)        
         
-        
+
+def course_add(c_name, dep, c_id, unit, c_field):
+    try:
+        Q1 ="INSERT INTO Teacher (c_name, dep, c_id, unit, c_field) VALUES(%s, %s, %s, %s, %s)"
+        values = (c_name, dep, c_id, unit, c_field)
+        cursor.execute(Q1, values)
+        db.commit()
+    except Exception as e:
+        print(e)
+   
+   
 student_add("danial", "jahed", 1, 23, "mashhad", "software", "m", 18.26)
 student_add("ahmad", "ghadirzadeh", 2, 23, "mashhad", "software", "m", 17.36)
 student_add("mahsa", "ghanad", 3, 20, "shiraz", "hardware", "f", 15.13)
