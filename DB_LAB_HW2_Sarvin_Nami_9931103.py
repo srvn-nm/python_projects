88ffe317ba836d0f7af513e6750a5a6f2c998432
Sarvin Nami <srvn0nm@gmail.com>
2022-12-19 22:11:01 +0330

    first query

--- DB_LAB_HW2_Sarvin_Nami_9931103.py
+++ DB_LAB_HW2_Sarvin_Nami_9931103.py
@@ -118,3 +118,14 @@ term_add(12, 5, 8, 4, 9.25, 6)
 term_add(13, 8, 5, 6, 8.75, 6)
 term_add(14, 15, 2, 2, 16, 4)
 term_add(15, 14, 3, 7, 15, 7)
+
+
+print("-----------------------------------------------------------------------")
+Q1 = "SELECT Student.s_name, Student.s_family FROM Student INNER JOIN term ON ((age >= 20) and (Term.s_id = Student.s_id) and (Term.id = 3))"
+cursor.execute(Q1)
+records = cursor.fetchall()
+print("Total number of rows in table of query 1: ", cursor.rowcount)
+print("\nPrinting each row")
+for row in records:
+    print("Name = ", row[0], )
+    print("Family = ", row[1], "\n\n****\n")
