a3ee03636ad46cedbd1a705d821f17522b8b2895
Sarvin Nami <srvn0nm@gmail.com>
2022-12-20 17:43:33 +0330

    writing number generator

--- DA_HW2_Q1.py
+++ DA_HW2_Q1.py
@@ -1,11 +1,27 @@
 def fullPrime(n):
-    if (n == 1):
-        return 0
-         
-    i = 2
-    while i * i <= n and n > 1 :
-        if (n % i == 0):
+    while n > 1:
+        if (n == 1):
             return 0
-        i += 1
-        n %= 10
+         
+        i = 2
+        while i * i <= n :
+            if (n % i == 0):
+                return 0
+            i += 1
+        
+        n /= 10
     return 1
+
+
+def printFullPrime(n):
+    num = pow(10,(n - 1))
+    while num <= (pow(10,n)-1):
+        if fullPrime(num) == 1:
+            print(num)
+        num += 1
+    
+    
+    
+    
+    
+printFullPrime(3)
