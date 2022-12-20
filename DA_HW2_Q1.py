71a0d669965c9f1e47ea664338d9d66b11015afa
Sarvin Nami <srvn0nm@gmail.com>
2022-12-20 18:20:25 +0330

    using dynamic programing

--- DA_HW2_Q1.py
+++ DA_HW2_Q1.py
@@ -1,30 +1,37 @@
 # import time
 # start_time = time.time()
-numbers = []
+fullPrimeNumbers = []
+notFullPrimeNumbers = []
 def fullPrime(n):
-    while n > 0:
-        if (n == 1):
-            return 0
-         
-        i = 2
-        while i * i <= n :
-            if (n % i == 0):
+    if fullPrimeNumbers.count(n) == 0 and notFullPrimeNumbers.count(n) != 0:
+        notFullPrimeNumbers.append(n)
+        return 0
+    elif fullPrimeNumbers.count(n) != 0 and notFullPrimeNumbers.count(n) == 0:
+        fullPrimeNumbers.append(n)
+        return 1
+    elif fullPrimeNumbers.count(n) != 0 and notFullPrimeNumbers.count(n) != 0:
+        while n > 0:
+            if (n == 1):
+                notFullPrimeNumbers.append(n)
                 return 0
-            i += 1
+            
+            i = 2
+            while i * i <= n :
+                if (n % i == 0):
+                    notFullPrimeNumbers.append(n)
+                    return 0
+                i += 1
+            n = int(n/10)
         
-        n = int(n/10)
+    fullPrimeNumbers.append(n)
     return 1
 
 
 def printFullPrime(n):
     num = pow(10,(n - 1))
     while num <= (pow(10,n)-1):
-        if numbers.count(fullPrime(num)) == 0:
-            if fullPrime(num) == 1:
-                numbers.append(fullPrime(num))
-                print(num)
-        else:
-            print(num)
+        if fullPrime(num) == 1:
+             print(num)
         num += 1
     
     
