62c5f6b70a762b853a70dde712dcf94cd738e815
Sarvin Nami <srvn0nm@gmail.com>
2022-12-26 19:31:23 +0330

    writing query 2

--- DB_LAB_HW3_Sarvin_Nami_9931103.py
+++ DB_LAB_HW3_Sarvin_Nami_9931103.py
@@ -1,1 +1,2 @@
 Q1 = "SELECT Student.Sname, Student.Sfamily, Student.city, College.clg_name FROM Student INNER JOIN College ON Student.#clg = College.#clg WHERE Student.city != College.city"
+Q2 = "SELECT Proof.Pname, Proof.Pfamily FROM Proof INNER JOIN Sec"
