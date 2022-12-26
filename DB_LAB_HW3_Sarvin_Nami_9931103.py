147b128276170ca67d935e4ad2a2594efa10b7d0
Sarvin Nami <srvn0nm@gmail.com>
2022-12-26 20:51:17 +0330

    writing query 3

--- DB_LAB_HW3_Sarvin_Nami_9931103.py
+++ DB_LAB_HW3_Sarvin_Nami_9931103.py
@@ -1,2 +1,3 @@
 Q1 = "SELECT Student.Sname, Student.Sfamily, Student.city, College.clg_name FROM Student INNER JOIN College ON Student.#clg = College.#clg WHERE Student.city != College.city"
 Q2 = "SELECT Proof.Pname, Proof.Pfamily FROM Proof INNER JOIN Sec INNER JOIN Course ON Proof.#P = Sec.#P and Course.#C = Sec.#C WHERE Course.Cname = 'database' and Sec.term = 1 and Sec.year = 1401"
+Q3 = "SELECT Student.Sname, Student.Sfamily,Course.Cname FROM Student INNER JOIN Course INNER JOIN Sec ON Student.#S = Sec.#S and Sec.#C =Course.#C WHERE Course.#clg != Student.#clg"
