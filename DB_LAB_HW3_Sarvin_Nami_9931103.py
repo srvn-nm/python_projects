1acfca7205a940480d6ff0c97ec9da2ea10702bc
Sarvin Nami <srvn0nm@gmail.com>
2022-12-26 20:05:25 +0330

    debugging

--- DB_LAB_HW3_Sarvin_Nami_9931103.py
+++ DB_LAB_HW3_Sarvin_Nami_9931103.py
@@ -1,2 +1,2 @@
 Q1 = "SELECT Student.Sname, Student.Sfamily, Student.city, College.clg_name FROM Student INNER JOIN College ON Student.#clg = College.#clg WHERE Student.city != College.city"
-Q2 = "SELECT Proof.Pname, Proof.Pfamily FROM Proof INNER JOIN Sec"
+Q2 = "SELECT Proof.Pname, Proof.Pfamily FROM Proof INNER JOIN Sec INNER JOIN Course ON Proof.#P = Sec.#P and Course.#C = Sec.#C WHERE Course.Cname = 'database' and Sec.term = 1 and Sec.year = 1401"
