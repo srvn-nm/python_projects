a69cc82e87e23d59acf48c9e705ba9f7ce877981
Sarvin Nami <srvn0nm@gmail.com>
2022-12-30 18:55:25 +0330

    debugging

--- DA_HW2_Q1.py
+++ DA_HW2_Q1.py
@@ -1,24 +1,25 @@
-import time
-start_time = time.time()
-import functools
-fullPrimeNumbers, notFullPrimeNumbers = set(), set()
-@functools.lru_cache(maxsize=300)
-
+# import time
+# start_time = time.time()
+fullPrimeNumbers = set()
+notFullPrimeNumbers = set()
 def fullPrime(n):
-    i = 5
-    w = 2
-    while n > 0:
-        if (n in fullPrimeNumbers and not n in notFullPrimeNumbers) or (n == 1 or n % 2 == 0 or n % 3 == 0 or n < 0):
-            return 0
-        elif (not n in fullPrimeNumbers and n in notFullPrimeNumbers) or (n == 2 or n == 3):
-            return 1
-        while i * i <= n :
-            if (n % i == 0):
+    if n in fullPrimeNumbers and not n in notFullPrimeNumbers:
+        return 1
+    elif not n in fullPrimeNumbers and n in notFullPrimeNumbers:
+        return 0
+    else:
+        while n > 0:
+            if (n == 1):
                 notFullPrimeNumbers.add(n)
                 return 0
-            i += w
-            w = 6 - w
-        n = (int(n/10))
+            
+            i = 2
+            while i * i <= n :
+                if (n % i == 0):
+                    notFullPrimeNumbers.add(n)
+                    return 0
+                i += 1
+            n = int(n/10)
         
     fullPrimeNumbers.add(n)
     return 1
@@ -26,7 +27,9 @@ def fullPrime(n):
     
     
 n = int(input())
-for num in range(pow(10,(n - 1)),(pow(10,n))):
-        if fullPrime(num) == 1:
-             print(num)
-print("--- %s seconds ---" % (time.time() - start_time))
+num = pow(10,(n - 1))
+while num <= (pow(10,n)-1):
+    if fullPrime(num) == 1:
+            print(num)
+    num += 1
+# print("--- %s seconds ---" % (time.time() - start_time))
