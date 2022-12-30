e2476c720513322f53e6350073f626f3d50829cd
Sarvin Nami <srvn0nm@gmail.com>
2022-12-30 20:16:33 +0330

    debugging

--- DA_HW2_Q2.py
+++ DA_HW2_Q2.py
@@ -1,96 +1,50 @@
-def merge(arr, temp, l, m, r) :
- 
-    # i: index to left subarray
-    i = l
+def bucketSort(arr, noOfBuckets):
+    max_ele = max(arr)
+    min_ele = min(arr)
   
-    # j: index to right subarray
-    j = m + 1
+    # range(for buckets)
+    rnge = (max_ele - min_ele) / noOfBuckets
   
-    # Stores count of pairs that
-    # satisfy the given condition
-    cnt = 0
-    for l in range(m + 1) :
-        found = False
+    temp = []
   
-        # Traverse to check for the
-        # valid conditions
-        while (j <= r) :
+    # create empty buckets
+    for i in range(noOfBuckets):
+        temp.append([])
   
-            # If condition satisfies
-            if (arr[i] > arr[j]) :
-                found = True        
-            else :
-                break
-            j += 1
+    # scatter the array elements
+    # into the correct bucket
+    for i in range(len(arr)):
+        diff = (arr[i] - min_ele) / rnge - int((arr[i] - min_ele) / rnge)
   
-        # While a[i] > a[j] satisfies
-        # increase j
+        # append the boundary elements to the lower array
+        if(diff == 0 and arr[i] != min_ele):
+            temp[int((arr[i] - min_ele) / rnge) - 1].append(arr[i])
   
-        # All elements in the right
-        # side of the left subarray
-        # also satisfies
-        if (found) :
-            cnt += j - (m + 1)
-            j -= 1
+        else:
+            temp[int((arr[i] - min_ele) / rnge)].append(arr[i])
   
-    # Sort the two given arrays and
-    # store in the resultant array
-    k = l
-    i = l
-    j = m + 1
+    # Sort each bucket individually
+    for i in range(len(temp)):
+        if len(temp[i]) != 0:
+                for i in range(1, len(temp)):
+                    up = temp[i]
+                    j = i - 1
+                while j >= 0 and temp[j] > up: 
+                    temp[j + 1] = temp[j]
+                    j -= 1
+                temp[j + 1] = up     
   
-    while (i <= m and j <= r) :
-        if (arr[i] <= arr[j]) :
-            temp[k] = arr[i]
-            k += 1
-            i += 1
-        else :
-            temp[k] = arr[j]
-            k += 1
-            j += 1
+    # Gather sorted elements 
+    # to the original array
+    k = 0
+    for lst in temp:
+        if lst:
+            for i in lst:
+                arr[k] = i
+                k = k+1
   
-    # Elements which are left
-    # in the left subarray
-    while (i <= m) :
-        temp[k] = arr[i]
-        k += 1
-        i += 1
   
-    # Elements which are left
-    # in the right subarray
-    while (j <= r) :
-        temp[k] = arr[j]
-        k += 1
-        j += 1
-    for i in range(l, r + 1) :
-        arr[i] = temp[i]
-  
-    # Return the count obtained
-    return cnt
-  
-# Function to partition array into two halves
-def mergeSortUtil(arr, temp, l, r) :
-    cnt = 0
-    if (l < r) :
-  
-        # Same as (l + r) / 2, but avoids
-        # overflow for large l and h
-        m = (l + r) // 2
-  
-        # Sort first and second halves
-        cnt += mergeSortUtil(arr, temp, l, m)
-        cnt += mergeSortUtil(arr, temp, m + 1, r)
-  
-        # Call the merging function
-        cnt += merge(arr, temp, l, m, r)
-    return cnt
-  
-# Function to print the count of
-# required pairs using Merge Sort
-def mergeSort(arr, N) :
-    temp = [0]*N
-    print(mergeSortUtil(arr, temp, 0, N - 1)%100000)
- 
-  # Driver code
+# Driver Code
 arr = [ 3, 2, 3, 1 ]
-mergeSort(arr, len(arr))
+bucketSort(arr, int(len(arr)/2))
+print(arr)
