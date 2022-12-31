05569d6ad8bba6912563dcebc18432753c40fd42
Sarvin Nami <srvn0nm@gmail.com>
2022-12-31 15:17:54 +0330

    debugging

--- DA_HW2_Q2.py
+++ DA_HW2_Q2.py
@@ -1,44 +1,21 @@
-def mergeSort(arr, n):
-    temp_arr = [0]*n
-    return _mergeSort(arr, temp_arr, 0, n-1)
- 
-def _mergeSort(arr, temp_arr, left, right):
-    inv_count = 0
-    if left < right:
-        mid = (left + right)//2
-        inv_count += _mergeSort(arr, temp_arr, left, mid)
-        inv_count += _mergeSort(arr, temp_arr, mid + 1, right)
-        inv_count += merge(arr, temp_arr, left, mid, right)
-    return inv_count
- 
-def merge(arr, temp_arr, left, mid, right):
-    i = left    
-    j = mid + 1  
-    k = left     
-    inv_count = 0
-    while i <= mid and j <= right:
-        if arr[i] <= arr[j]:
-            temp_arr[k] = arr[i]
-            k += 1
-            i += 1
-        else:
-            temp_arr[k] = arr[j]
-            inv_count += (mid-i + 1)
-            k += 1
-            j += 1
-    while i <= mid:
-        temp_arr[k] = arr[i]
-        k += 1
-        i += 1
-    while j <= right:
-        temp_arr[k] = arr[j]
-        k += 1
-        j += 1
-    for loop_var in range(left, right + 1):
-        arr[loop_var] = temp_arr[loop_var]
-    return inv_count
+from heapq import heappush, heappop
+from bisect import bisect, insort
+def getNumOfInversions(A):
+    N = len(A)
+    if N <= 1:
+        return 0
+    sortList = []
+    result = 0
+    for i, v in enumerate(A):
+        heappush(sortList, (v, i))
+    x = []
+    while sortList:
+        v, i = heappop(sortList)
+        y = bisect(x, i)
+        result += i - y
+        insort(x, i)
+    return result
  
 arr = [ 3, 2, 3, 1 ]
-n = len(arr)
-result = mergeSort(arr, n)
-print("Number of inversions are", result)
+result = getNumOfInversions(arr)%100000
+print(f'Number of inversions are {result}')
