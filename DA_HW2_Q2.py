371e57741a99739d2ca1e45d7b61f947b85d1c9d
Sarvin Nami <srvn0nm@gmail.com>
2023-01-02 13:05:20 +0330

    debugging

--- DA_HW2_Q2.py
+++ DA_HW2_Q2.py
@@ -1,62 +1,57 @@
 # import time
 # start_time = time.time()
-def MS(arr, n):  
-    temp_arr = [0]*n  
-    return merge_sort(arr, temp_arr, 0, n-1)  
-  
-def merge_sort(arr, temp_arr, l, r):  
-    ic = 0  
-  
-    if l < r:  
-  
-        mid = (l + r)//2  
-  
-        ic = ic +  merge_sort(arr, temp_arr, l, mid)  
-  
-        ic = ic + merge_sort(arr, temp_arr, mid + 1, r)  
-  
-        ic = ic + merge(arr, temp_arr, l, mid, r)  
-    return ic  
-
-  
-def merge(arr, temp, l, m, r):  
-    i = l   
-    j = m + 1 
-    k = l    
-    ic = 0  
-
-    while i <= m and j <= r:  
-  
-          
-        if arr[i] <= arr[j]:  
-            temp[k] = arr[i]  
-            k = k + 1  
-            i = i + 1  
-        else:   
-            temp[k] = arr[j]  
-            ic = ic + (m-i + 1)  
-            k = k + 1  
-            j = j + 1  
-
-    while i <= m:  
-        temp[k] = arr[i]  
-        k = k + 1  
-        i = i + 1  
-
-    while j <= r:  
-        temp[k] = arr[j]  
-        k = k + 1  
-        j = j + 1  
-    for n in range(l, r + 1):  
-        arr[n] = temp[n]  
-  
-    return ic  
-  
-  
-# Given array is  
-arr = []
+def merge(arr, left, mid, right):
+    i = left
+    j = mid
+    k = 0
+    invCount = 0
+    temp = [0 for x in range(right - left + 1)]
+ 
+    while (i < mid) and (j <= right):
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
+            k += 1
+            i += 1
+ 
+        else:
+            temp[k] = arr[j]
+            invCount += mid - i
+            k += 1
+            j += 1
+ 
+    while i < mid:
+        temp[k] = arr[i]
+        k += 1
+        i += 1
+ 
+    while j <= right:
+        temp[k] = arr[j]
+        k += 1
+        j += 1
+ 
+    k = 0
+    for i in range(left, right + 1):
+        arr[i] = temp[k]
+        k += 1
+ 
+    return invCount
+ 
+ 
+def mergeSort(arr, left, right):
+    invCount = 0
+ 
+    if right > left:
+        mid = (right + left) // 2
+ 
+        invCount = mergeSort(arr, left, mid)
+        invCount += mergeSort(arr, mid + 1, right)
+        invCount += merge(arr, left, mid + 1, right)
+ 
+    return invCount
+ 
 n = int(input())  
+arr = [0]*n
 for i in range(n): 
     arr[i] = int(input())
-print(MS(arr, n)%100000)
+print(mergeSort(arr, 0, n - 1)%100000)
 # print("--- %s seconds ---" % (time.time() - start_time))
