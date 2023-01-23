922105208e49c47da280f3f8c12d37bd1f55fbc6
Sarvin Nami <srvn0nm@gmail.com>
2023-01-23 21:27:15 +0330

    debugging

--- DA_HW4_Q1.py
+++ DA_HW4_Q1.py
@@ -2,12 +2,24 @@ def add_edge(adj, src, dest, d):
  
     adj[src][dest] = d
     adj[dest][src] = d
+
+paths=[]
+def find_path(adj,start,end,minPath):
     
-    
+    for i in adj:
+        if i == start:
+            for j in adj:
+                if j == adj[i].keys():
+                    minPath += adj[i][j]
+                    if j == end:
+                        paths.append(minPath)
+                        return 
+                    else:
+                        return find_path(adj,j,end,minPath)
     
 mn = input()
-m = int(mn[0])
-n = int(mn[2])
+m = int(mn[2])
+n = int(mn[0])
 adj = [{} for i in range(n)]
 for i in range(m):
     uvd = input()
@@ -15,3 +27,10 @@ for i in range(m):
     v = int(uvd[2])
     d = int(uvd[4])
     add_edge(adj,u,v,d)
+find_path(adj, 0, n-1, 0)
+minPath = min(paths)
+res = 0
+for i in paths:
+    if paths[i] == minPath:
+        res += 1
+print(res)
