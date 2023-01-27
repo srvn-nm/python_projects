1e05c1814d98222fefe397dc475a9a4a52ea2824
Sarvin Nami <srvn0nm@gmail.com>
2023-01-27 13:53:16 +0330

    beautify the code

--- DA_HW4_Q2.py
+++ DA_HW4_Q2.py
@@ -3,10 +3,7 @@ import sys
 class Graph():
   
     def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
-        
+        self.V, self.graph = vertices, [[0 for column in range(vertices)]for row in range(vertices)]
         
     def calculateMST(self, parent):
         res = 0
@@ -15,41 +12,34 @@ class Graph():
         return res
     
     def add_edge(self, u, v, d):
-        self.graph[u][v] = d
-        self.graph[v][u] = d
+        self.graph[u][v], self.graph[v][u] = d, d
     
     def minKey(self, key, mstSet):
         min = sys.maxsize
         for v in range(self.V):
             if key[v] < min and mstSet[v] == False:
-                min = key[v]
-                min_index = v
+                min, min_index = key[v], v
         return min_index
     
     def primMST(self):
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V  
-        key[0] = 0
-        mstSet = [False] * self.V
-        parent[0] = -1 
+        key, parent, mstSet = [sys.maxsize] * self.V, [None] * self.V, [False] * self.V
+        key[0], parent[0] = 0, -1 
         for cout in range(self.V):
             u = self.minKey(key, mstSet)
             mstSet[u] = True
             for v in range(self.V):
                 if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
+                    key[v], parent[v] = self.graph[u][v], u
         return self.calculateMST(parent)
 
 
 nm = input().split(" ")
-m, n= int(nm[1]),int(nm[0])
+m, n, firstSum= int(nm[1]),int(nm[0]), 0
 g = Graph(n)
 for i in range(m):
     uvd = input().split(" ")
     g.add_edge(int(uvd[0]),int(uvd[1]),int(uvd[2]))
 
-firstSum = 0
 for row in g.graph:
     for w in row:
         firstSum += w
