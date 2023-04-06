ae5ddf599f4030cd1600945256d2afa82f6888c9
Sarvin Nami <srvn0nm@gmail.com>
2023-04-06 23:54:41 +0430

    normalizing the data

--- InformationRetrieval/BrowserProject.py
+++ InformationRetrieval/BrowserProject.py
@@ -1,9 +1,16 @@
+from __future__ import unicode_literals
 import json
+from hazm import *
 
 
 docs_contents = []
 docs_titles = []
 docs_urls = []
+positionalIndex = {}
+docs_ranks = {}
+phrase = []
+stopWords_list = ["با", "و", "در", "ولی", "اما", "نیز", "اگر", "که", "مگر", "از", "بر", "تا", "بی", "الا", "غیر", ".",
+                ",", "،", ".", "/", "را", "مانند", "جزو", ":", "به", "؛"]
 
 def read():
     f = open('IR_data_news_12k.json', encoding='utf8')
@@ -13,3 +20,18 @@ def read():
         docs_contents.append(data[i]["content"])
         docs_urls.append(data[i]["url"])
     f.close()
+    
+def normalize(data, i):
+    normalizer = Normalizer()
+    data = normalizer.normalize(data)
+    tokenizedData = word_tokenize(data)
+    stemmer = Stemmer()
+    for j in range(len(tokenizedData)):
+        tokenizedData[j] = stemmer.stem(tokenizedData[j])
+    for i in tokenizedData:
+        if i in stopWords_list:
+            tokenizedData.remove(i)
+    index(tokenizedData, i)
+    
+def index():
+    pass
