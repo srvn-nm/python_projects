f0703e15a23d26e85122cffe60cc5373edfdcb87
Sarvin Nami <srvn0nm@gmail.com>
2023-04-06 22:01:08 +0430

    adding  read function to read the data file and store it

---
+++ InformationRetrieval/BrowserProject.py
@@ -0,0 +1,12 @@
+docs_contents = []
+docs_titles = []
+docs_urls = []
+
+def read():
+    f = open('IR_data_news_12k.json', encoding='utf8')
+    data = json.load(f)
+    for i in data:
+        docs_titles.append(data[i]["title"])
+        docs_contents.append(data[i]["content"])
+        docs_urls.append(data[i]["url"])
+    f.close()
