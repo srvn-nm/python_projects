42a88b368e86e729b1789b3b4ea35e5aece0ab8a
Sarvin Nami <srvn0nm@gmail.com>
2023-06-11 20:53:16 +0330

    debugging

--- InformationRetrieval/BrowserProject.py
+++ InformationRetrieval/BrowserProject.py
@@ -7,7 +7,7 @@ from math import *
 
 from hazm import *
 
-docsContents, docsTitles, docsUrls, positionalIndex, docsRanks, phrases, stopWordsList, champion = {}, [], [], [], {}, {}, [], ["با",
+docsContents, docsTitles, docsUrls, positionalIndex, docsRanks, phrases, stopWordsList, champion = [], [], [], {}, {}, {}, [], ["با",
                                                                                                                   "و",
                                                                                                                   "در",
                                                                                                                   "ولی",
