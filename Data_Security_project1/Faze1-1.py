6f914ce6e123b316cc0ddbf4607e3553a53d01ed
Sarvin Nami <srvn0nm@gmail.com>
2023-11-15 15:00:49 +0330

    security strength grader

---
+++ Data_Security_project1/Faze1-1.py
@@ -0,0 +1,78 @@
+import argparse
+# import getpass
+import string
+
+parser = argparse.ArgumentParser(description="Password Security Assessment Tool")
+parser.add_argument("password", help="Password")
+
+args = parser.parse_args()
+
+
+def check_password_strength(password):
+    lower_alpha_count = upper_alpha_count = number_count = special_char_count = length = common = 0
+
+#lowercase, uppercase, digits
+    for char in list(password):
+        if char in string.ascii_lowercase:
+            lower_alpha_count += 1
+        elif char in string.ascii_uppercase:
+            upper_alpha_count += 1
+        elif char in string.digits:
+            number_count += 1
+        else:
+            special_char_count += 1
+    
+#dictionary check
+    with open("10-million-password-list-top-1000000.txt", 'r') as file:
+        content = file.read()
+
+        if password not in content:
+            common += 1
+            
+
+#length of the password
+    if len(password) >= 12:
+       length += 1 
+       
+#strength grading
+    strength = 0
+
+    if lower_alpha_count >= 1:
+        strength += 1
+
+    if upper_alpha_count >= 1:
+        strength += 1
+
+    if number_count >= 1:
+        strength += 1
+
+    if special_char_count >= 1:
+        strength += 1
+
+    if length == 1:
+        strength += 1
+
+    if common == 1:
+        strength += 1
+        
+    return strength
+
+
+# max_attempts = 3  # Maximum unsuccessful authentication attempts
+
+# try:
+        # Security algorithms - Rate limiting
+        # attempts = 0
+        # while attempts < max_attempts:
+            # user_input = getpass.getpass("Please enter your password again: ")
+            # if user_input == args.password:
+            #     print("Authentication successful.")
+            #     break
+            # else:
+            #     print("Incorrect password. Please try again.")
+            #     attempts += 1
+
+# except argparse.ArgumentTypeError as e:
+#         print(f"Error: {e}")
+grade = check_password_strength(args.password)
+print("The security strength grade is " + str(grade) + " out of 7! : " + str(grade/7))
