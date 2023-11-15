c1503c6022df916dd707b69db17a48dffcefaea2
Sarvin Nami <srvn0nm@gmail.com>
2023-11-15 23:05:06 +0330

    debugging of the faze2 for key generation

--- Data_Security_project1/Faze2.py
+++ Data_Security_project1/Faze2.py
@@ -1,9 +1,26 @@
+import argparse
+import base64
+
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
 
 def generate_key(password):
-    # تولید کلید براساس گذرواژه
-    return Fernet.generate_key()
 
+    # Derive a key from the password using PBKDF2
+    salt = b'salt_'  # Salt value to make the key unique
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,  # Length of the key
+        salt=salt,
+        iterations=100000,  # Number of iterations
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    return key
 def encrypt_file(file_path, key):
     with open(file_path, 'rb') as file:
         data = file.read()
@@ -25,18 +42,19 @@ def decrypt_file(file_path, key):
         file.write(decrypted_data)
 
 def main():
-    mode = int(input("Please choose mode: (1: Encryption, 2: Decryption): "))
+    parser = argparse.ArgumentParser(description="File Encryption and Decryption Tool")
+    parser.add_argument("mode", type=int, choices=[1, 2], help="Select mode (1: Encryption, 2: Decryption)")
+    parser.add_argument("file_path", help="Path of the file")
+    parser.add_argument("password", help="Password for encryption/decryption")
 
-    password = input("Enter password for encryption/decryption: ")
-    key = generate_key(password.encode())
+    args = parser.parse_args()
+    key = generate_key(args.password.encode())
 
-    if mode == 1:
-        file_path = input("Enter the path of the file to encrypt: ")
-        encrypt_file(file_path, key)
+    if args.mode == 1:
+        encrypt_file(args.file_path, key)
         print("File encrypted successfully.")
-    elif mode == 2:
-        file_path = input("Enter the path of the file to decrypt: ")
-        decrypt_file(file_path, key)
+    elif args.mode == 2:
+        decrypt_file(args.file_path, key)
         print("File decrypted successfully.")
     else:
         print("Invalid mode. Please select 1 for encryption or 2 for decryption.")
