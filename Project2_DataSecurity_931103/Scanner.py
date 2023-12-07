4e8c9782f6d5da467a2e1f62847424c846984826
Sarvin Nami <srvn0nm@gmail.com>
2023-12-07 16:26:16 +0330

    refactor

--- Project2_DataSecurity_931103/Scanner.py
+++ Project2_DataSecurity_931103/Scanner.py
@@ -2,7 +2,7 @@ import argparse
 import socket
 import struct
 
-#function for scanning the ip range
+# Function for scanning the IP range
 def scan_ip_range(start_ip, end_ip, subnet_mask):
     active_machines = []
 
@@ -13,7 +13,6 @@ def scan_ip_range(start_ip, end_ip, subnet_mask):
     num_addresses = 2 ** (32 - subnet_bits)
 
     network_address = start & end
-#   wildcard = (2 ** 32 - 1) - (2 ** (32 - subnet_bits) - 1)
 
     for i in range(num_addresses):
         current_ip = socket.inet_ntoa(struct.pack("!I", network_address + i))
@@ -28,28 +27,27 @@ def scan_ip_range(start_ip, end_ip, subnet_mask):
 
     return active_machines
 
-
-#function for scanning open ports
+# Function for scanning open ports
 def scan_open_ports(ip_address, start_port, end_port, protocol):
     open_ports = []
 
-    # تعیین نوع پروتکل برای سوکت
+#type of protocol
     if protocol.lower() == 'tcp':
-        socket_type = socket.SOCK_STREAM  # برای TCP
+        socket_type = socket.SOCK_STREAM
     elif protocol.lower() == 'udp':
-        socket_type = socket.SOCK_DGRAM  # برای UDP
+        socket_type = socket.SOCK_DGRAM
     else:
         print("Invalid protocol")
         return open_ports
 
-    # Loop اسکن برای هر پورت در محدوده مشخص شده
+#scan for available ports in the loop
     for port in range(start_port, end_port + 1):
         try:
             sock = socket.socket(socket.AF_INET, socket_type)
-            sock.settimeout(1)  # تنظیم تایم‌اوت برای اتصال سریع‌تر
+            #timeout for quicker connection
+            sock.settimeout(1)
             result = sock.connect_ex((ip_address, port))
 
-            # اگر نتوان به پورت متصل شد، پورت بسته است
             if result != 0:
                 print("Port {} is closed".format(port))
             else:
@@ -58,11 +56,12 @@ def scan_open_ports(ip_address, start_port, end_port, protocol):
 
             sock.close()
         except socket.error:
-            pass  # اگر خطایی رخ داد، به پورت بعدی برو
+            #if any error occurs, go to the next port
+            pass
 
     return open_ports
 
-#function for saving into a.txt file
+# Function for saving into a .txt file
 def save_report_to_txt(report, file_path):
     try:
         with open(file_path, 'w') as file:
@@ -92,14 +91,13 @@ def main():
 
     if args.portscan:
         if args.tcp:
-            scan_open_ports('127.0.0.1', args.tcp[0], args.tcp[1], 'TCP')
-            save_report_to_txt(report=args.tcp, file_path="C/prj2.txt")
+            report = scan_open_ports('127.0.0.1', args.tcp[0], args.tcp[1], 'TCP')
+            save_report_to_txt(report=report, file_path="C:/prj2.txt")
         elif args.udp:
-            scan_open_ports('127.0.0.1', args.udp[0], args.udp[1], 'UDP')
-            save_report_to_txt(report=args.udp, file_path="C/prj2.txt")
+            report = scan_open_ports('127.0.0.1', args.udp[0], args.udp[1], 'UDP')
+            save_report_to_txt(report=report, file_path="C:/prj2.txt")
         else:
             print("Please specify TCP or UDP port range")
-
 
 if __name__ == "__main__":
     main()
