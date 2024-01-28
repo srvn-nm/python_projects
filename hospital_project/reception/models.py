6994fbfff12593bcbfa2266208c7ed8251363f02
Sarvin Nami <srvn0nm@gmail.com>
2024-01-29 00:08:12 +0330

    reception models

--- hospital_project/reception/models.py
+++ hospital_project/reception/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bed(models.Model):
+    occupied = models.BooleanField(default=False)
+    patient_name = models.CharField(max_length=255, blank=True, null=True)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=255)
