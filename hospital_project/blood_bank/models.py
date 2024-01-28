ca941bf3f9aecb24eae1295af6462ede59080de8
Sarvin Nami <srvn0nm@gmail.com>
2024-01-29 00:06:45 +0330

    bank blood models

--- hospital_project/blood_bank/models.py
+++ hospital_project/blood_bank/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BloodInventory(models.Model):
+    blood_group = models.CharField(max_length=2)  # Assuming only A, B, AB, O without +/-
+    quantity_liters = models.IntegerField(default=0)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=255)
+    blood_group = models.CharField(max_length=2)
+    blood_required_liters = models.IntegerField()
+    days_in_hospital = models.IntegerField()
