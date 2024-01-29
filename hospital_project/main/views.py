fb6cef5825dac627afd55d088352ab73356a3feb
Sarvin Nami <srvn0nm@gmail.com>
2024-01-29 10:46:32 +0330

    create main view

--- hospital_project/main/views.py
+++ hospital_project/main/views.py
@@ -1,3 +1,22 @@
+# from django.db.models import Sum
 from django.shortcuts import render
+# from reception.models import Bed
+# from blood_bank.models import BloodInventory, Patient
 
-# Create your views here.
+
+def main(request):
+    # Fetch relevant data for the system overview
+    # total_beds = Bed.objects.count()
+    # empty_beds = Bed.objects.filter(patient__isnull=True).count()
+    # total_blood_liters = BloodInventory.objects.aggregate(total=Sum('blood_liters'))['total']
+    # total_patients = Patient.objects.count()
+
+    # context = {
+    #     'total_beds': total_beds,
+    #     'empty_beds': empty_beds,
+    #     'total_blood_liters': total_blood_liters,
+    #     'total_patients': total_patients,
+    # }
+
+    return render(request, 'main/main_page.html')
+    # , context)
