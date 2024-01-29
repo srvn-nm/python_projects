0d170002765ce613ca6de0501064e12615e4ae62
Sarvin Nami <srvn0nm@gmail.com>
2024-01-29 10:46:10 +0330

    adding main url

--- hospital_project/hospital_project/urls.py
+++ hospital_project/hospital_project/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('reception/', include('reception.urls')),
     path('blood_bank/', include('blood_bank.urls')),
+    path('', include('main.urls')),
     # Add other app URLs here
 ]
