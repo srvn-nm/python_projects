64615f0d3c100913157dcd6beb339e4810a6ec0f
Sarvin Nami <srvn0nm@gmail.com>
2024-01-29 10:46:24 +0330

    create main url

--- hospital_project/main/urls.py
+++ hospital_project/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from main.views import main
+
 urlpatterns = [
     path('reception/', include('reception.urls')),
     path('blood_bank/', include('blood_bank.urls')),
+    path('', main, name='main_view'),
 ]
